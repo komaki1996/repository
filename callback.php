33ed7489955b4f76e2819aa159dc4c79fec634d4
komaki1996 <komaki1996@yahoo.co.jp>
2016-07-12 00:59:28 +0900

    Update callback.php

--- callback.php
+++ callback.php
@@ -3,14 +3,14 @@
 require_once('LineBot.php');
 
 // LINE:チャンネルID
-$CHANNEL_ID = '[LINEのチャンネルID]';
+$CHANNEL_ID = 1462842097;
 // LINE:チャンネルシークレット
-$CHANNEL_SECRET = '[LINEのチェンネルシークレット';
+$CHANNEL_SECRET = komaki99;
 // LINE:MID
-$CHANNEL_MID = '[LINEのMID]';
+$CHANNEL_MID = uba13cc7aa65749b5de894379523112f9;
 
 // Bingアカウントキー
-$ACCOUNT_KEY = '[Bing Search APIのアカウントキー]';
+$ACCOUNT_KEY = Y9Ij42lqUC0uWblnfKjp2glRuWi8Du2nx9btUi/LtZQ;
 
 $bot = new LineBot($CHANNEL_ID, $CHANNEL_SECRET, $CHANNEL_MID);
 $bot->sendText('「%s」デスネ...');
